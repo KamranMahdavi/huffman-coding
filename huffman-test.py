ceb01c5ab8dd53d35a3a784e89b2fb10058dfe27
KamranMahdavi <Kamranz.mahdaviz@gmail.com>
2026-05-03 20:48:42 -0300

    Updated test script to be interactive

--- huffman-test.py
+++ huffman-test.py
@@ -1,6 +1,6 @@
 from huffman import huffman_encode, huffman_decode
 
-text = "Huffman test"
+text = input("Enter your text: ")
 
 encoded_text, encoded_tree = huffman_encode(text)
 decoded_text = huffman_decode(encoded_text, encoded_tree)
